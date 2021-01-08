09a996b7ae039068a2663c6a728bb2c460085d36
Martin <s182935@student.dtu.dk>
2021-01-08 11:13:16 +0100

    Bug fix: Hvis man ikke slår 2 ens ender ens tur 1.0

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -256,19 +256,20 @@ public class GameController {
                     EndGame();
                     if (GameOver) break;
 
+                    if (!playerList[i].getInJail()) {
+                        //Guibutton to read the next user input
+                        String ready = uiController.getGUI().getUserButtonPressed(uiController.getGuiPlayer(i).getName() + currentLang[14], currentLang[15]);
+                        // if statement to check if the user typed in throw
+                        if (ready.equals(currentLang[15])) {
+                            //Change die on in gui to reflect new roll and update player position
+                            rafflecup.useRafflecup();
+                            uiController.getGUI().setDice(d1.getFaceValue(), d2.getFaceValue());
+                            playerList[i].setPosition(30);
 
-                    //Guibutton to read the next user input
-                    String ready = uiController.getGUI().getUserButtonPressed(uiController.getGuiPlayer(i).getName() + currentLang[14], currentLang[15]);
-                    // if statement to check if the user typed in throw
-                    if (ready.equals(currentLang[15]) && !playerList[i].getInJail()) {
-                        //Change die on in gui to reflect new roll and update player position
-                        rafflecup.useRafflecup();
-                        uiController.getGUI().setDice(d1.getFaceValue(), d2.getFaceValue());
-                        playerList[i].setPosition(30);
+                            //updates gui player position
+                            uiController.updateGUIPlayerPos(playerList[i], playerList[i].getOldposition(), playerList[i].getPosition());
 
-                        //updates gui player position
-                        uiController.updateGUIPlayerPos(playerList[i], playerList[i].getOldposition(), playerList[i].getPosition());
-
+                        }
                     }
                     //Part 1 of landOnField test see part 2
                     //System.out.println(playerList[i].getName() + " before landing on field: " + playerList[i].getMoney());
