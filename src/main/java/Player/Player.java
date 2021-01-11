a741eb71ffc49b6fde75b59e5b8e1f9b79cc13fb
zPadduz <jul2009@live.dk>
2021-01-11 17:39:14 +0100

    fikset fejl med crash

--- src/main/java/Player/Player.java
+++ src/main/java/Player/Player.java
@@ -130,7 +130,7 @@ public class Player{
         int playerAsset = 0;
         if(playerOwnedFields.current > 0) {
             for (int i = 0; i < playerOwnedFields.size; i++) {
-                Field tempField = fields[playerOwnedFields.getArr()[i]];
+                Field tempField = fields[playerOwnedFields.atIndex(i)];
                 if (tempField instanceof Properties) {
                     playerAsset += ((Properties) tempField).getTotalPropertyValue();
                 } else if (tempField instanceof Brewery) {
