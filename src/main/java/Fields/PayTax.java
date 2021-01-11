ee6dca8f9ee12cce0e01905c671d676acf9943ae
Sealnpenguin <sealnpenguin@protonmail.com>
2021-01-11 12:02:09 +0100

    call on totalPropertyValue has been added to PayTax

--- src/main/java/Fields/PayTax.java
+++ src/main/java/Fields/PayTax.java
@@ -12,4 +12,10 @@ public class PayTax extends Field{
     public void landOnField(Player[] players, int player) {
 
     }
+
+    public void landOnField(Player[] players, int player, Field[] fields) {
+        int totalValue = 0;
+        totalValue += ((Properties)fields[1]).getTotalPropertyValue(players, player, fields);
+
+    }
 }
