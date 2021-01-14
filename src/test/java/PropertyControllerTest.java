20b52700254a87836f391397d9d6a7499876f54f
Sealnpenguin <sealnpenguin@protonmail.com>
2021-01-14 10:19:06 +0100

    changed the name of a maven depency from CDIO3 to CDIO4

--- src/test/java/PropertyControllerTest.java
+++ src/test/java/PropertyControllerTest.java
@@ -1,23 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
-import Fields.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PropertyControllerTest {
-//6, 8 , 9
+
     @Test
-    void constructTest(){
-       FieldsOnBoard f1 = new FieldsOnBoard();
-       PropertyController controller = new PropertyController(f1.getFieldArr());
-       //Changing the owner for group 1 to test functionality
-       ((Properties)controller.getFields()[1]).setOwnedBy(1);
-        ((Properties)controller.getFields()[3]).setOwnedBy(1);
-        ((Properties)controller.getFields()[9]).setOwnedBy(1);
-        //used to generate posibillities
-       //Used the get the generated posibillities
-       Field[] tester = controller.getBuyingPosibillites(1);
-       tester = controller.getBuyingPosibillites(2);
-       int i = 0;
+    void getBuyingPosibillites() {
     }
 
+    @Test
+    void getPawningPossibilites() {
+    }
 
+    @Test
+    void getSellingPossibilities() {
+    }
 }
